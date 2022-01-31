fa365bd084429df90fe0dca05fbb403450c31845
vinicoloco <vinicius.inacio1997@gmail.com>
2022-01-31 12:10:47 -0300

    Mudanças no código.

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,31 +1,32 @@
 function contar() {
-    let ini = window.document.getElementById('txti')
-    let fim = window.document.getElementById('txtf')
-    let passo = window.document.getElementById('txtp')
-    let res = window.document.getElementById('res')
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
 
-    if(ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
-        res.innerHTML = 'Impossível contar.'
-        //window.alert('ERRO, faltam dados!!!')     
-    }else{
-        res.innerHTML = 'Contando: <br>'
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+        res.innerHTML = 'Impossivel contar.'
+        window.alert('[ERRO] Faltam dados!')
+    } else {
+        res.innerHTML = 'Contando... <br>'
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
         if (p <= 0){
-            window.alert('Passo inválido!')
+            window.alert('Passo inválido! Considerando passo 1.')
+            p = 1
         }
-        if (i < f){
-            //Contagem crescente.
-            for(let c = i; c <= f; c += p){
-                res.innerHTML += `${c} \u{1F449}  `
+        if (i < f) {
+            //contagem crescente
+            for(let c = i; c <= f; c += p) {
+                res.innerHTML += ` ${c} \u{1F449}`
             }
-        }else{
-            //Contagem regressiva.
-            for (let c = i; c >= f; c -= p){
-            res.innerHTML += `${c} \u{1F449}`
-            }
-        }  
-        res.innerHTML += `\u{1F3C1}`
+        }else {
+            //contagem regressiva
+            for(c = i; c >= f; c -= p) {
+                res.innerHTML += ` ${c} \u{1F449}`  
+            } 
+        }
+       res.innerHTML += ' \u{1F3C1}'
     }
 }
